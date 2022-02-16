00d01c49d4081230949958bb510e27660ef2e8a5
Salamander-m <74792658+Salamander-m@users.noreply.github.com>
2022-02-16 16:36:52 +0300

    updated

--- python andreev/2_1.py
+++ python andreev/2_1.py
@@ -1,6 +1,9 @@
 from math import *
 from random import *
 m = []
+k = 0
+z = 0
+maximum = 0
 row = int(input())
 line = int(input())
 for i in range(row):
@@ -8,4 +11,14 @@ for i in range(row):
     for j in range(line):
         a.append(uniform(-5,5))
     m.append(a)
+    if 0 not in a:
+        k = k + 1
+    b = sorted(a,reverse=True)
+    z = 0
+    for j in range(line):
+        if b[j] >= maximum:
+            z = z + 1
+            maximum = b[j]
+print('Количество повторяющего максимума',z)
+print('Количество строк, не содержащих ни одного нулевого элемента',k)
 print(m)
